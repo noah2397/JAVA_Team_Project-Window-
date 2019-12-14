53d1dbccdf3bc85c7556f48d488b3a5d53d99afd
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2019-12-14 20:17:15 +0900

    Add files via upload

--- test.c
+++ test.c
@@ -11,28 +11,51 @@
 
 #include <math.h> //sin함수 쓰기위해 사용
 #define PI 3.141592
-void* print_clock(void *f){
-	int way = (int) f;
-	
-	
+float sine(int); //함수 선언
+float cosine(int);
 
+void* print_clock(void *f){
+	char* way = (char*) f;
+	float sin,cos,a,b;
+	if(strcmp(way,"sec")!=0){
+		for(int i=0; i<=360; i++){
+
+        		sin = sine(i)*15+30;
+			cos = cosine(i)*30+60;
+			mvaddstr(30,60, "*");
+			mvaddstr((int)sin, (int)cos, "*");
+
+			usleep(50000);
+			refresh();
+		}
+	}
+	if(strcmp(way,"min")!=0){
+		for(int i=0; i<=360; i++){
+
+        		sin = sine(i)*15+30;
+			cos = cosine(i)*30+60;
+			mvaddstr(30,60, "*");
+			mvaddstr((int)a, (int)b, "0");
+
+			usleep(50000);
+			refresh();
+		}
+	}
 
 
 
 }
-float sine(int); //함수 선언
-float cosine(int);
+
 int main(void){
 	initscr();
 	clear();
 	float sin,cos;
 	float a,b;
 	float c,d;
-
 	pthread_t t1,t2;
 	void *print_clock(void*);
-	pthread_create(&t1, NULL, print_clock, (void*)0);
-	pthread_create(&t2, NULL, print_clock, (void*)1);
+	pthread_create(&t1, NULL, print_clock, (void*)"sec");
+	pthread_create(&t2, NULL, print_clock, (void*)"min");
 	for(int i=0; i<=360; i++){
 
         	sin = sine(i)*17+30;
@@ -40,19 +63,8 @@ int main(void){
 		mvaddstr((int)sin, (int)cos, "0");
 		refresh();
 	}
-	for(int i=0; i<=360; i++){
-
-        	sin = sine(i)*15+30;
-		cos = cosine(i)*30+60;
-        	a = sine(i)*10+30;
-		b = cosine(i)*20+60;
-		mvaddstr(30,60, "*");
-		mvaddstr((int)sin, (int)cos, "*");
-		mvaddstr((int)a, (int)b, "0");
-
-		usleep(50000);
-		refresh();
-	}
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
 	endwin();
 
 		
@@ -77,27 +89,5 @@ float cosine(int sec)
 	return temp;
         
 }
-/*void main(void) {
-	signal(SIGQUIT,SIG_IGN);
-	int today, yo, month,year;
-	int unit;
-	time_t timer;
-	struct tm *t;
-	int input;
-
-	timer = time(NULL);
-	t = localtime(&timer);
-	
-	
-	month = t->tm_mon + 1;
-	year = t->tm_year + 1900;
-
-
-    	int i;
-	
-
-
-	
-}*/
 
 
